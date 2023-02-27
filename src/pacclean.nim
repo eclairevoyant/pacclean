34e80c366d9e6080009090ae2850c07db094d1a9
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2023-02-26 23:58:32 -0500

    output human-readable total file size (stderr)

--- src/pacclean.nim
+++ src/pacclean.nim
@@ -1,7 +1,9 @@
-import std/[algorithm,os,sequtils,strutils,sugar,tables]
+import std/[algorithm,math,os,sequtils,strformat,strutils,sugar,tables]
 
 func alpm_pkg_vercmp(a: cstring; b: cstring): cint {.dynlib: "libalpm.so", importc.}
 func verCmp(a: string, b: string): int = alpm_pkg_vercmp(a, b)
+
+func `/`(x, y: BiggestInt): BiggestFloat = toBiggestFloat(x) / toBiggestFloat(y)
 
 func pkginfo(s: string): (string, string, string) =
   # don't split more than 3 times, because package names can have '-'
@@ -9,6 +11,20 @@ func pkginfo(s: string): (string, string, string) =
   if (splits.len > 3):
     return (splits[0], splits[1], splits[2])
   return ("", "", "")
+
+func humanReadable(i: BiggestInt): (BiggestFloat, string) =
+  let ifloat = toBiggestFloat(i)
+  if (i >= (2 ^ 50)):
+    return (i / (2 ^ 50), "P")
+  if (i >= (2 ^ 40)):
+    return (i / (2 ^ 40), "T")
+  if (i >= (2 ^ 30)):
+    return (i / (2 ^ 30), "G")
+  if (i >= (2 ^ 20)):
+    return (i / (2 ^ 20), "M")
+  if (i >= (2 ^ 10)):
+    return (i / (2 ^ 10), "K")
+  return (toBiggestFloat(i), "")
 
 let 
   fileList = collect(newSeq):
@@ -42,7 +58,15 @@ for p in packageMap.keys:
   packageMap[p].delete(0..0)
   toDelete = toDelete.concat(packageMap[p].map(x => x[1]))
 
+var totalSize: BiggestInt
 for p in toDelete:
   echo p
+  totalSize += getFileSize(p)
   if fileList.contains(p & ".sig"):
     echo p & ".sig"
+    totalSize += getFileSize(p & ".sig")
+
+let (totalSizeHuman, totalSizePrefix) = humanReadable(totalSize)
+
+# TODO opt to show/hide total filesize
+stderr.writeLine(fmt"Total file size: {totalSizeHuman:0.1f} {totalSizePrefix}B")
