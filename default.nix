6e7a4cbf6c0e60216c4f67c272a1701636730163
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2023-07-15 21:16:58 -0400

    nix: build docs; reformat(alejandra)

--- default.nix
+++ default.nix
@@ -1,15 +1,33 @@
-{ lib, autoPatchelfHook, fetchFromGitHub, nimPackages, pacman }:
-
-nimPackages.buildNimPackage(finalAttrs: {
+{
+  lib,
+  autoPatchelfHook,
+  fetchFromGitHub,
+  nimPackages,
+  pandoc,
+  pacman,
+}:
+nimPackages.buildNimPackage (finalAttrs: rec {
   pname = "pacclean";
   version = "0.0.1";
   src = ./.;
   meta = {
     description = "Clean up old pacman packages";
     homepage = "https://github.com/eclairevoyant/pacclean";
-    license = lib.licenses.cc-by-nc-sa-40;
+    #license = lib.licenses.cc-by-nc-sa-40;
     platforms = lib.platforms.linux;
   };
-  nativeBuildInputs = [ autoPatchelfHook ];
-  buildInputs = [ pacman ];
+  nativeBuildInputs = [autoPatchelfHook pandoc];
+  buildInputs = [pacman];
+  buildPhase = ''
+    runHook preBuild
+    nim_builder --phase:build
+    pandoc -s -o ${pname}.1 doc/pacclean.man1.md
+    runHook postBuild
+  '';
+  installPhase = ''
+    runHook preInstall
+    nim_builder --phase:install
+    install -Dm644 ${pname}.1 -t "$out/share/man/man1/"
+    runHook postInstall
+  '';
 })
--- flake.nix
+++ flake.nix
@@ -5,11 +5,12 @@
     nixpkgs.url = "github:nixos/nixpkgs";
   };
 
-  outputs = { self, nixpkgs }@inputs:
-  let
+  outputs = {
+    self,
+    nixpkgs,
+  } @ inputs: let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in
-  {
+  in {
     packages.x86_64-linux.default = pkgs.callPackage ./. {};
   };
 }
