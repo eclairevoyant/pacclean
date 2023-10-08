7232e54d1aeb91672f6ac1e000e11026e023553e
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2023-10-08 04:02:36 -0400

    clean up nix derivation

--- default.nix
+++ default.nix
@@ -6,29 +6,27 @@
   pandoc,
   pacman,
 }:
-nimPackages.buildNimPackage (finalAttrs: rec {
+nimPackages.buildNimPackage (finalAttrs: {
   pname = "pacclean";
   version = "0.0.1";
   src = ./.;
+
   meta = {
     description = "Clean up old pacman packages";
     homepage = "https://github.com/eclairevoyant/pacclean";
     #license = lib.licenses.cc-by-nc-sa-40;
     platforms = lib.platforms.linux;
   };
+
   nativeBuildInputs = [autoPatchelfHook pandoc];
   buildInputs = [pacman];
   hardeningEnable = ["pie"];
-  buildPhase = ''
-    runHook preBuild
-    nim_builder --phase:build
-    pandoc -s -o ${pname}.1 doc/pacclean.man1.md
-    runHook postBuild
+
+  postBuild = ''
+    pandoc -s -o ${finalAttrs.pname}.1 doc/pacclean.man1.md
   '';
-  installPhase = ''
-    runHook preInstall
-    nim_builder --phase:install
-    install -Dm644 ${pname}.1 -t "$out/share/man/man1/"
-    runHook postInstall
+
+  postInstall = ''
+    install -Dm644 ${finalAttrs.pname}.1 -t "$out/share/man/man1/"
   '';
 })
