dcf671612d636055cff9804f9bc0368fa51c514a
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2023-10-08 04:13:59 -0400

    make derivation (almost) nixpkgs-ready

--- default.nix
+++ default.nix
@@ -1,26 +1,28 @@
 {
   lib,
-  autoPatchelfHook,
-  fetchFromGitHub,
   nimPackages,
-  pandoc,
+  fetchFromGitHub,
+  autoPatchelfHook,
   pacman,
+  pandoc,
 }:
+
 nimPackages.buildNimPackage (finalAttrs: {
   pname = "pacclean";
   version = "0.0.1";
+
   src = ./.;
 
-  meta = {
-    description = "Clean up old pacman packages";
-    homepage = "https://github.com/eclairevoyant/pacclean";
-    #license = lib.licenses.cc-by-nc-sa-40;
-    platforms = lib.platforms.linux;
-  };
+  nativeBuildInputs = [
+    autoPatchelfHook
+    pandoc
+  ];
 
-  nativeBuildInputs = [autoPatchelfHook pandoc];
-  buildInputs = [pacman];
-  hardeningEnable = ["pie"];
+  buildInputs = [
+    pacman
+  ];
+
+  hardeningEnable = [ "pie" ];
 
   postBuild = ''
     pandoc -s -o ${finalAttrs.pname}.1 doc/pacclean.man1.md
@@ -29,4 +31,13 @@ nimPackages.buildNimPackage (finalAttrs: {
   postInstall = ''
     install -Dm644 ${finalAttrs.pname}.1 -t "$out/share/man/man1/"
   '';
+
+  meta = {
+    description = "Clean up old pacman packages";
+    homepage = "https://github.com/eclairevoyant/pacclean";
+    #license = lib.licenses.cc-by-nc-sa-40;
+    mainProgram = "pacclean";
+    maintainers = with lib.maintainers; [ eclairevoyant ];
+    platforms = lib.platforms.linux;
+  };
 })
